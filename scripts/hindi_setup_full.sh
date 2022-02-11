cb2dc056f143285579b82b2e3445dda3fafdc780
PratikGarai <pratikgarai0208@gmail.com>
2022-02-11 12:44:02 +0530

    hindi full setup script

---
+++ scripts/hindi_setup_full.sh
@@ -0,0 +1,5 @@
+sh scripts/hindi/hindi_weight_downloader.sh
+sh scripts/hindi/hindi_model_creator.sh
+sh scripts/hindi/hindi_model_packer.sh
+sh scripts/hindi/hindi_model_installer.sh
+sh scripts/hindi/hindi_linker.sh
